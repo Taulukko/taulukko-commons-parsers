58247a693e6878a35082a7e42b91497517cf8cae
gandb <gandbranco@gmail.com>
2017-03-04 11:47:18 -0300

    v 1.0.7 - corrigido erro causado por generics

---
+++ system/src/test/groovy/com/taulukko/commons/parsers/ConfigBean.java
@@ -0,0 +1,5 @@
+package com.taulukko.commons.parsers;
+
+public class ConfigBean {
+	public Boolean verbose = false; 
+}
---
+++ system/src/test/groovy/com/taulukko/commons/parsers/InjectTemplateTest.groovy
@@ -0,0 +1,19 @@
+package com.taulukko.commons.parsers;
+
+import org.junit.Test
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.taulukko.commons.parsers.jsonParser.JSONParser
+
+@RunWith(JUnit4.class)
+class InjectTemplateTest {
+
+	@Test
+	void jsonToClass () {
+		String json ='{"verbose": true }';
+		ConfigBean config = JSONParser.convert(json, ConfigBean.class);
+		assert config != null;
+		assert config.verbose == true
+	}
+}
